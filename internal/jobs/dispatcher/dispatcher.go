7525d327e7f82a534c5835ed3cefedf2a2a7baf6
puneet-jr <puneetjanakiram@gmail.com>
2026-06-17 01:32:29 +0530

    Started adding RabbitMQ

--- internal/jobs/dispatcher/dispatcher.go
+++ internal/jobs/dispatcher/dispatcher.go
@@ -2,9 +2,15 @@ package dispatcher
 
 import "context"
 
-// JobDispatcher abstracts the message queue. 
-// In production, this would be implemented by Redis, RabbitMQ, or Kafka.
-type JobDispatcher interface {
+// JobMessage wraps the payload and the acknowledgement functions.
+// This allows the worker to tell the message broker when a job is truly done.
+type JobMessage struct {
+	JobID string
+	Ack   func() error
+	Nack  func(requeue bool) error
+}
+
+type Dispatcher interface {
 	Enqueue(ctx context.Context, jobID string) error
-	Subscribe() <-chan string
+	Jobs(ctx context.Context) <-chan JobMessage
 }
